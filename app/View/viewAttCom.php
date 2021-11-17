9e10571cf0ccecc3b1a5031acbcc8ce061ce0539
2M691381 <91610821+2M691381@users.noreply.github.com>
2021-11-17 22:21:23 +0100

    Add files via upload

---
+++ app/View/viewAttCom.php
@@ -0,0 +1,9 @@
+<?php $this->title = 'Félicitation, v^tre commentaire est en attente de modération'; ?>
+
+<div class="container" id="viewCongrat">
+	<h2><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> MERCI pour ce commentaire ! Nous allons maintenant devoir le valider ou non sous 24H . </h2>
+
+	<div>
+	    Dans l'attente de la validation de votre commentaire, vous pouvez commenter mes derniers articles en <a href="index.php?action=posts"> cliquant ici</a><br />
+	</div>
+</div>
